c98ef48504d7f01a444750503b217dc77868db13
mohan <manimohan.arichandiran@adcltech.com>
2021-12-17 18:47:18 +0530

    as seen on

--- src/components/offerbottom.js
+++ src/components/offerbottom.js
@@ -13,8 +13,9 @@ const Offerbottom = () => {
         const splash = async () => {
           await axios({
            method : "get",
-           url: `${process.env.GATSBY_CART_URL}admin/footerbanner/`
+           url: `${process.env.GATSBY_CART_URL_STARCARE}category/asseenonbanner`
            }).then((res) => {
+             console.log(res)
            const item = res.data
            setSplash(item);
             setLoader(false);
@@ -28,10 +29,10 @@ const Renderproduct = () => {
     if (splash) { 
 
         return <>
-        {splash.map((data) => (
+        {splash.slice(0 ,2).map((data) => (
         (
       <div className="col-lg-6 col-md-6 col-xs-12">
-          <Link to={data.button_link} ><img src={ad1} alt={"banner"} /></Link> 
+          <Link to={data.category_url} ><img src={data.category_image} alt={"banner"} /></Link> 
           </div>
            )))}
 </>
@@ -44,16 +45,16 @@ return(
     <div className="row">
     <div className="col-lg-12 col" >
   <h2 className="section_title">
-                    <span>Featured Products</span>
-                    <span><Link to="/featuredProducts">+ View all Products</Link></span>
+                    <span>As Seen On</span>
+                    {/* <span><Link to="/featuredProducts">+ View all Products</Link></span> */}
                     </h2>
                     </div>
                     </div>
   <div className="row">
-  
+{/*   
   <div className="col-lg-6 col-md-6 col-xs-12"><img src={ad1} alt={"banner"} /></div>
-  <div className="col-lg-6 col-md-6 col-xs-12"><img src={ad2} alt={"banner"} /></div>
-    {/* {Renderproduct()} */}
+  <div className="col-lg-6 col-md-6 col-xs-12"><img src={ad2} alt={"banner"} /></div> */}
+    {Renderproduct()}
     </div>
   </div>
      </section>
