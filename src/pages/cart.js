9bb7cb7ab06a30ad10b37735665336af8b1c91c3
Gokulkrishna33 <sgokulkrishna3@gmail.com>
2022-01-20 19:35:45 +0530

    cartupt

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -16,29 +16,80 @@ const Cart = () => {
     const [checkOut, setCheckout] = useState([]);
     const [updCart, setupdCart] = useState("");
     const [jwt, setjwt] = useState();
-    const [subtotal, setsubtotal] = useState("");
+    const [status, setstatus] = useState([]);
+    const [productid, setproductid] = useState([]);
+    const [customerId, setCustomerId] = useState("");
+
+
 
     useEffect(() => {
+        setCustomerId(localStorage.customer_id)
         setjwt(localStorage.userToken)
         // if (checkLogin()) {
-            if (!checkLogin()) {
-            navigate('/signin')
-          } else {
-            viewCartItems();
-              if(localStorage.getItem('cartData')){
-                let parseCart = JSON.parse(localStorage.getItem('cartData'));
-                if (parseCart) {
-                    setCartItems(JSON.parse(localStorage.getItem('cartData')));
-                    if(localStorage.getItem('userToken')){
-                        fetchCheckTotal();
-                    }
+            fetchCheckTotal();
+            listCarts();
+            //   if(localStorage.getItem('cartData')){
+            //     let parseCart = JSON.parse(localStorage.getItem('cartData'));
+            //     if (parseCart) {
+            //         setCartItems(JSON.parse(localStorage.getItem('cartData')));
+            //         console.log(JSON.parse(localStorage.getItem('cartData')))
+            //         if(localStorage.getItem('userToken')){
+            //             fetchCheckTotal();
+            //         }
                    
-                }
-              }
-    }
-    }, [])
-   
+            //     }
+            //   }
+    
+
+    }, []);
+    const listCarts = () => {
+            let resi=[]
+            let stocks=[]
+            const jwt = localStorage.getItem('userToken');
+            const email = localStorage.email;
+            try {
+              axios({  
+                  method : 'get',
+                  url : `${process.env.GATSBY_CART_URL_STARCARE}mycartitems/${email}`,
+                  headers : {
+                         'Authorization' : `Bearer ${jwt}`
+                       }  
+                }).then(async (res) => {
+                  
+                      console.log(res.data)
+                      for(let i=0;i<res.data.length;i++){
+                        const resp = await fetch(
+                          `${process.env.GATSBY_CART_URL_STARCARE}cart/productstatus/product_id/${res.data[i].product_id}`
+                      );
+                      resi.push(await resp.json());
+                      }
+                      for(let i=0;i<resi.length;i++){
+                        await stocks.push(resi[i][0]['stock_status'])
+                      }
+                     await res.data.forEach(object => {
+                        object.status = stocks;
+                      });
+                      console.log(res.data)
+                      const data = JSON.stringify(res.data)
+                      localStorage.setItem('cartData' , JSON.stringify(res.data))
+                      setCartItems(JSON.parse(data))
+                  
+          
+                  
+                  
+                }).catch((err) =>{
+                  alert('error occured')
+                  console.error(err)
+                })
+              
+            } catch (err) {
+                console.error(err)
+            }
+          
+      }
+
     const fetchCheckTotal = async () => {
+       
         const jwt = localStorage.getItem('userToken')
         try {
             axios({
@@ -77,94 +128,96 @@ const Cart = () => {
         }
     }
 
-    const handleChange = (event) => {
+    const handleChange = (event,data,ivalue) => {
         if (event.target.value <= 0) {
             event.target.value = 1;
-            setupdCart(event.target.value);
-            
+            data.qty = event.target.value;
            
         } else {
             setupdCart(event.target.value)
-            
+            data.qty = event.target.value;
             
         }
 
     }
 
+    const addToList = (type,id) => {
+        // type 1 = wishlist
+        // type 2 = comparelist
+        let url = (type == 1 ? `${process.env.GATSBY_CART_URL_STARCARE}admin/addtocompare/2` : `${process.env.GATSBY_CART_URL_STARCARE}wishlist/addwishlist_product/`)
+        let message = (type == 1 ? 'Sucessfully added to  compare list' : 'Sucessfully added to wish list')
+        let errormessage = (type == 1 ? 'SignIn to add compare list' : 'SignIn to add wish list')
 
-    const updateCart = (item) => {
-        let updateItem;
-        if (updCart === '') {
-            updateItem = {
-                "cartItem": {
-                    "item_id": item.item_id,
-                    "qty": item.qty,
-                    "quote_id": item.quote_id
-                }
-            }
-        } else {
-            updateItem = {
-                "cartItem": {
-                    "item_id": item.item_id,
-                    "qty": parseInt(updCart),
-                    "quote_id": item.quote_id
-                }
-            }
+        let productData = {
+          "data": {
+            "customer_id": customerId,
+            "product_id": id
+          }
         }
-
-
+    
         try {
-            axios({
-                method: "put",
-                url: `${process.env.GATSBY_CART_URL_STARCARE}carts/mine/items/${item.item_id}`,
-                headers: {
-                    'Authorization': `Bearer ${jwt}`
-                },
-                data: updateItem
-            }).then((response) => {
-                if (response.statusText === "OK" && response.status == 200) {
-                    fetchCheckTotal()
-                    viewCartItems1()
-                    
-                    toast.success("Updated sucessfully")
-                }
-            }).catch((err) => {
-                console.error(err)
-                toast.error('Failed to update')
-            })
+          axios({
+            method: 'post',
+            url: url,
+            data: productData,
+            headers: {
+              'Authorization': `Bearer ${jwt}`
+            }
+          }).then((res) => {
+            if (res.statusText === "OK" && res.status == 200) {
+              toast.success(message)
+              setTimeout(()=>{
+               // wishListCount()
+              },2000)
+            }
+          }).catch((err) => {
+            toast.error(errormessage)
+          })
+        } catch (err) {
+          toast.error(err)
         }
-        catch (err) {
-            console.error(err)
-        }
-    }
-    const viewCartItems1 = () => {
-        const jwt = localStorage.getItem('userToken');
-        const email = localStorage.email;
-        try{
-          axios({  
-              method : 'get',
-              url : `${process.env.GATSBY_CART_URL_STARCARE}mycartitems/${email}`,
-              headers : {
-                     'Authorization' : `Bearer ${jwt}`
-                   }  
-            }).then((res) => {
-              if(res.statusText === "OK" && res.status == 200){
-                  const data = JSON.stringify(res.data)
-                  localStorage.setItem('cartData' , JSON.stringify(res.data))
-                  setCartItems(JSON.parse(data));
-              }
-      
-              return res;
-              
-            }).catch((err) =>{
-              alert('error occured')
-              console.error(err)
-            })
-          
-        }catch(err){
-            console.error(err)
-        }
+    
       }
+    const updateCarts = (item) => {
+        let updateItem;
+       for(let i=0;i<cartItems.length;i++){
+        updateItem = {
+            "cartItem": {
+                "item_id": cartItems[i].item_id,
+                "qty": cartItems[i].qty,
+                "quote_id": cartItems[i].quote_id
+            }
+        }
+   
+
+
+    try {
+        axios({
+            method: "put",
+            url: `${process.env.GATSBY_CART_URL_STARCARE}carts/mine/items/${cartItems[i].item_id}`,
+            headers: {
+                'Authorization': `Bearer ${jwt}`
+            },
+            data: updateItem
+        }).then((response) => {
+            if (response.statusText === "OK" && response.status == 200) {
+                fetchCheckTotal()
+                // viewCartItems1()
+                
+                // toast.success("Updated sucessfully")
+            }
+        }).catch((err) => {
+            console.error(err)
+            // toast.error('Failed to update')
+        })
+    }
+    catch (err) {
+        console.error(err)
+    }
+       }
+           
+    }
+    
 
     const showCartItems = () => {
 
@@ -189,14 +242,14 @@ const Cart = () => {
                       <td><img src={cart.image} /></td>
                       <td><p>{cart.product_name}</p></td>
                       <td>${parseFloat(cart.price).toFixed(2)}</td>
-                      <td><input type="number" name="qty" defaultValue={cart.qty} onChange={e => { handleChange(e, cart) }}/></td>
+                      <td><input type="number" name="qty" defaultValue={cart.qty} onChange={e => { handleChange(e, cart,index) }}/></td>
                         <td><p class="green">{cart.status[index]}</p></td>
                         <td><p>$ {cart.qty*cart.price}</p></td>
                        
                             <td> <div className="casualities">
                                 <a onClick={() => { resetCart(cart.item_id) }}> <AiTwotoneDelete /></a>
-                                <button className="btn btn heart" type="button" onClick={() => { updateCart(cart) }}><AiOutlineCloudUpload /></button>
-                                <button className="btn btn heart" type="button"><AiTwotoneHeart /></button>
+                                {/* <button className="btn btn heart" type="button" onClick={() => { updateCarts(cart) }}><AiOutlineCloudUpload /></button> */}
+                                <button className="btn btn heart" type="button" onClick={() => addToList(2,cart.product_id)}><AiTwotoneHeart /></button>
                             </div></td>
                     </tr>
                     </tbody>
@@ -276,7 +329,7 @@ const Cart = () => {
                                         <div className="col-lg-9 col-md-9 col-xs-12">
                                         <div className="casualities bottom">
                                 <a onClick={() => navigate('/')}> Continue Shopping  </a>
-                                {/*<button className="btn btn update" type="button"> Update Cart  </button>*/}
+                                <button className="btn btn update" type="button" onClick={() => updateCarts()}> Update Cart  </button>
                             </div>
                                         </div>
                                     </div>
