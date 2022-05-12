4b4af25a0f50c49681d8c9aa24ac7da100521f2b
Gokulkrishna3 <gokul.krishna@adcltech.com>
2022-05-12 17:29:36 +0530

    categorysync1

---
+++ src/pages/categories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Router } from "@reach/router";
+import Products from "../templates/Products";
+import Layout from "../components/layout";
+
+const CategoriesPage = props => {
+  return (
+    
+      <div className="summa">
+        <Router>
+          <Products path="/categories/:slug" />
+        </Router>
+      </div>
+  );
+};
+
+export default CategoriesPage;
